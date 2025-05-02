345cb701fb6e64a59fa82285166abcac3555bce3
Ashok Neupane <ashokneupane100>
2025-05-02 18:58:23 +0545

    Custom Component Creation

--- assets/data/products.ts
+++ assets/data/products.ts
@@ -2,72 +2,62 @@ const products = [
   {
     id: 1,
     name: 'Ultimate Pepperoni',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: 12.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+    price: Math.round(12.99 * 133), // 1728 NPR
   },
   {
     id: 2,
     name: 'ExtravaganZZa',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 14.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+    price: Math.round(14.99 * 133), // 1994 NPR
   },
   {
     id: 3,
     name: 'MeatZZa',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
-    price: 13.47,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png',
+    price: Math.round(13.47 * 133), // 1792 NPR
   },
   {
     id: 4,
     name: 'Margarita',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
-    price: 9.9,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/margarita.png',
+    price: Math.round(9.9 * 133), // 1317 NPR
   },
   {
     id: 5,
     name: 'Pacific Veggie',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+    price: Math.round(12.99 * 133), // 1728 NPR
   },
   {
     id: 6,
     name: 'Hawaiian',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
-    price: 10.49,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/hawaiin.png',
+    price: Math.round(10.49 * 133), // 1395 NPR
   },
   {
     id: 7,
     name: 'Deluxe',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
-    price: 16.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/deluxe.png',
+    price: Math.round(16.99 * 133), // 2260 NPR
   },
   {
     id: 8,
     name: 'BBQ Chicken',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
-    price: 12.89,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/veggie.png',
+    price: Math.round(12.89 * 133), // 1714 NPR
   },
   {
     id: 9,
     name: 'Chicken Bacon Ranch',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
-    price: 13.99,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/extravaganzza.png',
+    price: Math.round(13.99 * 133), // 1861 NPR
   },
   {
     id: 10,
     name: '6 Cheese',
-    image:
-      'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
-    price: 13.29,
+    image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/6cheese.png',
+    price: Math.round(13.29 * 133), // 1768 NPR
   },
 ];
 
---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet, Text, View, Image } from "react-native";
+import Colors from "../constants/Colors";
+
+const ProductListItem = ({ product }) => {
+  console.log(product);
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name} </Text>
+      <Text style={styles.price}>Rs {product.price} </Text>
+    </View>
+  );
+};
+
+export default ProductListItem;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
