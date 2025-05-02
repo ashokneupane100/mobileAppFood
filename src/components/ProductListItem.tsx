2999063e4affb081a906d7154127f529af3acd07
Ashok Neupane <ashokabrother@gmail.com>
2025-05-03 05:37:25 +0545

    Creating Screens with Expo Router

--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, Text, View, Image, Pressable } from "react-native";
 import Colors from "../constants/Colors";
 import { Product } from "../types";
+import { Link } from "expo-router";
 
 export const defaultPizzaImage =
   "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png";
@@ -10,17 +11,18 @@ type ProductListItemProps = {
 };
 
 const ProductListItem = ({ product }: ProductListItemProps) => {
-  
   return (
-    <View style={styles.container}>
-      <Image
-        source={{ uri: product.image || defaultPizzaImage }}
-        style={styles.image}
-        resizeMode="contain"
-      />
-      <Text style={styles.title}>{product.name} </Text>
-      <Text style={styles.price}>Rs {product.price} </Text>
-    </View>
+    <Link href={`/${product.id}`} asChild>
+      <Pressable style={styles.container}>
+        <Image
+          source={{ uri: product.image || defaultPizzaImage }}
+          style={styles.image}
+          resizeMode="contain"
+        />
+        <Text style={styles.title}>{product.name} </Text>
+        <Text style={styles.price}>Rs {product.price} </Text>
+      </Pressable>
+    </Link>
   );
 };
 
@@ -31,8 +33,8 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 10,
     borderRadius: 20,
-    flex:1,
-    margin:10,
+    flex: 1,
+    margin: 10,
     maxWidth: "50%",
   },
   title: {
