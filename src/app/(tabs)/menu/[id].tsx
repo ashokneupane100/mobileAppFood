cb27d420e6b9c78e36abcdb66898745b55e3a0d9
Ashok Neupane <ashokabrother@gmail.com>
2025-05-04 06:12:13 +0545

    Select Size Component# 1.16

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,23 +1,79 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { StyleSheet, Text, View, Image } from "react-native";
+import React from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
 import products from "@assets/data/products";
+import { defaultPizzaImage } from "@components/ProductListItem";
+
+const sizes = ['S', 'M', 'L', 'XL'];
 
 const ProductDetailsScreen = () => {
-  const{id}=useLocalSearchParams();
+  const { id } = useLocalSearchParams();
 
-  const product=products.find((p)=>p.id.toString()===id);
+  const product = products.find((p) => p.id.toString() === id);
 
-
+  if (!product) {
+    return <Text>Product Not Found</Text>;
+  }
 
   return (
-    <View>
-      <Stack.Screen options={{title:product?.name}} />
-      <Text style={{fontSize:20}} >Product details id:{id} </Text>
+    <View style={styles.container}>
+      <Stack.Screen options={{ title: product.name }} />
+      <Image
+        source={{ uri: product.image || defaultPizzaImage }}
+        style={styles.image}
+      />
+
+      <Text style={styles.label}>Select Size:</Text>
+
+      <View style={styles.sizes}>
+        {sizes.map(size => 
+          <View key={size} style={styles.size}>
+            <Text style={styles.sizeText}>{size}</Text>
+          </View>
+        )}
+      </View>
+
+      <Text style={styles.price}>Rs {product.price}</Text>
     </View>
-  )
-}
+  );
+};
 
-export default ProductDetailsScreen
+export default ProductDetailsScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    flex: 1,
+    padding: 10,
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+  price: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  label: {
+    fontSize: 16,
+    fontWeight: "500",
+    marginBottom: 5,
+  },
+  sizes: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginVertical: 10,
+  },
+  size: {
+    backgroundColor: '#e0e0e0',
+    width: 50,
+    aspectRatio: 1,
+    borderRadius: 25,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  sizeText: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+});
